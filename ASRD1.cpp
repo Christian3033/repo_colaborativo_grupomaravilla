562b8600ee71a57c3304fd488d62f1df56c32298
AngDom <160678932+AngDom@users.noreply.github.com>
2024-03-03 12:31:29 -0600

    2doCommit

---
+++ ASRD1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using name space std;
+class ASRD{
+	private : string nombres, apellidos,direccion;
+	void crear ();
+	void leer ();
+	void actualizar();
+	void borrar ();
+	
+}; 
